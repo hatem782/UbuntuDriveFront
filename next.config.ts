b8dd0fd2f946466e8b2ad6b4e390ca875076c760
hatem782 <hatembenechikh100@gmail.com>
2025-04-21 17:42:13 +0200

    redirect to login + disabling reactStrictMode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: "/", // The page you want to redirect from (e.g., a protected route)
+        destination: "/login", // The page you want to redirect to (e.g., login page)
+        permanent: false, // Set to true if you want a permanent redirect (301), false for temporary (302)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
